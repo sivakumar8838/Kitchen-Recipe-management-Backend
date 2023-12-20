a031659a556cbcd23692f22fd5389e6526516476
siva <sivamss5882@gmail.com>
2023-12-20 16:36:31 +0530

    hi

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 
-const cors = require('cors');
-const express =require('express')
-const usersRouter = require('./controller/users');
-const loginRouter = require('./controller/login');
-const passwordRouter =require('./controller/passwordrese');
-const app = express();
-app.use(cors());
+// const cors = require('cors');
+// const express =require('express')
+// const usersRouter = require('./controller/users');
+// const loginRouter = require('./controller/login');
+// const passwordRouter =require('./controller/passwordrese');
+// const app = express();
+// app.use(cors());
 
-app.use(express.json());
+// app.use(express.json());
 
-app.use('/api/users', usersRouter);
-app.use('/api/login', loginRouter)
-app.use('/api/passwordreset', passwordRouter);
-module.exports = app;
+// app.use('/api/users', usersRouter);
+// app.use('/api/login', loginRouter)
+// app.use('/api/', passwordRouter);
+// module.exports = app;
